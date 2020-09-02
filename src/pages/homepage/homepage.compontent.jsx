b16b1c16a1af9397dbc92964f907eba28522f7cc
Cornelia Franta <cf@wh-i.at>
2020-09-02 15:30:48 +0200

    update our menu items to navigate with RouterHOC

--- src/pages/homepage/homepage.compontent.jsx
+++ src/pages/homepage/homepage.compontent.jsx
@@ -3,7 +3,7 @@ import Directory from "../../components/directory/directory.component";
 import './homepage.styles.sass'
 const HomePage = () => (
     <div className='homepage'>
-       <Directory/>
+       <Directory />
 
     </div>
 );
